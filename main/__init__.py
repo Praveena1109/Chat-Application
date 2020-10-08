47eb9b4eed040b5ef11023feb48cf76d627b242e
Praveena1109 <pravinaacharya2000@gmail.com>
2020-10-08 20:42:35 +0530

    style changes

--- main/__init__.py
+++ main/__init__.py
@@ -4,10 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_mail import Mail
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'c23761c3a31dc627ea1e638644ad7083'
+app.config['SECRET_KEY'] = ""  # ADD A SECRET CODE TO USE CSRF
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -19,10 +18,8 @@ socketio = SocketIO(app)
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
-
+app.config['MAIL_USERNAME'] = ""  # ADD YOUR EMAIL ID
+app.config['MAIL_PASSWORD'] = ""  # ADD YOUR PASSWORD
 mail = Mail(app)
 
 from main import routes
-
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from main import app, socketio,db
+from main import app, socketio, db
+import os
 
 db.create_all()
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="", port=5000)
+    socketio.run(app, debug=True, host=os.environ.get('HOST'), port=5000)
 
